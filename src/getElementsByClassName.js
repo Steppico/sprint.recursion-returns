92fcae4f0b8c91ff488454cbfbe16760f138d77d
go and Stefano <g.nakano555+steppashr@gmail.com>
2019-07-10 16:41:08 +0900

    Passed all test getElementsbyClassName except last

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,18 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (target) => {
+  const result = [];
+  function recursion(node) {
+    console.log(node.className.includes(target));
+    if (node.className === target) {
+      result.push(node);
+    }
+    if (node.childNodes) {
+      for (const child of node.childNodes) {
+        recursion(child);
+      }
+    }
+  }
+  recursion(document.body);
+  return result;
 };
 
 module.exports = { getElementsByClassName };
